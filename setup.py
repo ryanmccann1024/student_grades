e5e5088bf24c6286974235d786d4d53a1033723d
Ryan McCann <ryan_mccann@student.uml.edu>
2022-10-27 10:45:40 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first linear regression machine learning model to predict student grades.',
+    author='Ryan McCann',
+    license='MIT',
+)
